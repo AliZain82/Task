078b7f2f901c9349bd11fa9b379005e1c058f35e
AliZain82 <119394666+AliZain82@users.noreply.github.com>
2022-12-13 20:43:35 +0500

    Add files via upload

---
+++ HomeController.cs
@@ -0,0 +1,14 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+
+namespace AZ_project.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
+ 
